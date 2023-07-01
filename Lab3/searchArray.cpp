e01bd1217d5198e12e17e8da6dc62aefb182c77f
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 03:49:45 -0700

    Update searchArray.cpp

--- Lab3/searchArray.cpp
+++ Lab3/searchArray.cpp
@@ -1,50 +1,55 @@
 #include <iostream>
 
 using namespace std;
+
 int arr_size;
 int v;
 
-void search(int arr[], int key){
+void search(const int arr[], int key)
+{
     int count = 0;
-    for(int i = 0; i < arr_size; i++){
+    for (int i = 0; i < arr_size; i++)
+    {
         count++;
-        if(arr[i] == key){ // checks if key has been found
+        if (arr[i] == key)
+        {
             cout << "Found value " << key << " at index " << i << ", which took " << count << " checks." << endl;
-            if(count == 1){ // best scenario
+            if (count == 1)
+            {
                 cout << "We ran into the best case scenario!" << endl;
             }
-            if(count == arr_size){ // worst scenario
+            if (count == arr_size)
+            {
                 cout << "We ran into the worst case scenario!" << endl;
             }
-            break;
-        }
-        if(i == arr_size-1 && arr[i] != key){ // checks if value is not in array
-            cout << "The value " << key << " was not found in the array!" << endl;
+            return;
         }
     }
+    cout << "The value " << key << " was not found in the array!" << endl;
 }
 
-
-int main(){
+int main()
+{
     cout << "Enter the size of the array: ";
     cin >> arr_size;
 
-    if(arr_size < 0){ // error handling
+    if (arr_size < 0)
+    {
         cout << "ERROR: you entered an incorrect value for the array size!" << endl;
         return 0;
     }
 
-    // gets array from user
-    cout << "Enter the numbers in the array, seperated by a space, and press enter: " << endl;
+    cout << "Enter the numbers in the array, separated by a space, and press enter: " << endl;
     int arr[arr_size];
 
-    for(int i = 0; i < arr_size; i++){
-        scanf("%d", &arr[i]); // accepts integers (+ -) and inputs them into array
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i];
     }
 
-    cout << "Enter a number to seach for in the array: " << endl;
+    cout << "Enter a number to search for in the array: " << endl;
     cin >> v;
 
     search(arr, v);
-    return 0;  
+    return 0;
 }
