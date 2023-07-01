0e3af39ffadbf060665cfdea942ca9960c3770b1
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 03:43:06 -0700

    Update Exercise2.cpp

--- Lab10/Exercise2.cpp
+++ Lab10/Exercise2.cpp
@@ -3,121 +3,103 @@
 
 using namespace std;
 
-int main(){
-	Queue queue;
-	cout<<"Function Call\t\t"<<"Output\t\t"<<"Front  Queue  Rear"<<endl;
-	
+int main() {
+    Queue<char> queue;
+    cout << "Function Call\t\t" << "Output\t\t" << "Front  Queue  Rear" << endl;
 
-	cout<<"dequeue()\t\t";
-	try
-	{
-		if(queue.isEmpty()){
-			throw "exception";
-		}
-		else{
-			cout<<queue.dequeue();
-			cout<<"\t\t";
-			queue.print();
-			cout<<endl;
-		}
-	}
-	catch(...){
-		cout<<"exception"<<endl;
-	}
-	
+    cout << "dequeue()\t\t";
+    try {
+        if (queue.isEmpty()) {
+            throw "exception";
+        } else {
+            cout << queue.dequeue();
+            cout << "\t\t";
+            queue.print();
+            cout << endl;
+        }
+    } catch (...) {
+        cout << "exception" << endl;
+    }
 
-	cout<<"enqueue(D)\t\t";
-	queue.enqueue('D');
-	cout<<"\t\t";
-	queue.print();
-	cout<<endl;
+    cout << "enqueue(D)\t\t";
+    queue.enqueue('D');
+    cout << "\t\t";
+    queue.print();
+    cout << endl;
 
+    cout << "enqueue(A)\t\t";
+    queue.enqueue('A');
+    cout << "\t\t";
+    queue.print();
+    cout << endl;
 
-	cout<<"enqueue(A)\t\t";
-	queue.enqueue('A');
-	cout<<"\t\t";
-	queue.print();
-	cout<<endl;
+    cout << "dequeue()\t\t";
+    try {
+        if (queue.isEmpty()) {
+            throw "exception";
+        } else {
+            cout << queue.dequeue();
+            cout << "\t\t";
+            queue.print();
+            cout << endl;
+        }
+    } catch (...) {
+        cout << "exception" << endl;
+    }
 
+    cout << "size()\t\t\t";
+    cout << queue.size() << "\t\t";
+    queue.print();
+    cout << endl;
 
-	cout<<"dequeue()\t\t";
-	try{
-		if(queue.isEmpty()){
-			throw "exception";
-		}
-		else{
-			cout<<queue.dequeue();
-			cout<<"\t\t";
-			queue.print();					
-			cout<<endl;
-		}
-	}
-	catch(...){
-		cout<<"exception"<<endl;
-	}
+    cout << "enqueue(D)\t\t";
+    queue.enqueue('D');
+    cout << "\t\t";
+    queue.print();
+    cout << endl;
 
+    cout << "isEmpty()\t\t";
+    if (!queue.isEmpty()) {
+        cout << "false\t\t";
+    } else {
+        cout << "true\t\t";
+    }
+    queue.print();
+    cout << endl;
 
-	cout<<"size()\t\t\t";
-	cout<<queue.size()<<"\t\t";
-	queue.print();
-	cout<<endl;
+    cout << "front()\t\t\t";
+    try {
+        if (queue.isEmpty()) {
+            throw "exception";
+        } else {
+            cout << queue.front();
+            cout << "\t\t";
+            queue.print();
+            cout << endl;
+        }
+    } catch (...) {
+        cout << "Exception" << endl;
+    }
 
+    cout << "enqueue(T)\t\t";
+    queue.enqueue('T');
+    cout << "\t\t";
+    queue.print();
+    cout << endl;
 
-	cout<<"enqueue(D)\t\t";
-	queue.enqueue('D');
-	cout<<"\t\t";
-	queue.print();
-	cout<<endl;
-
-
-	cout<<"isEmpty()\t\t";
-	if(!queue.isEmpty()){
-		cout<<"false\t\t";
-	}
-	else{
-		cout<<"true\t\t";
-	}
-	queue.print();
-	cout<<endl;
-
-
-	cout<<"front()\t\t\t";
-	try{
-		if(queue.isEmpty())	{
-			throw "exception";
-		}
-		else{
-			cout<<queue.front();
-			cout<<"\t\t";
-			queue.print();					
-			cout<<endl;
-		}
-	}
-	catch(...){
-		cout<<"Exception"<<endl;
-	}
-
-
-	cout<<"enqueue(T)\t\t";
-	queue.enqueue('T');
-	cout<<"\t\t";
-	queue.print();
-	cout<<endl;
-
-
-	cout<<"front()\t\t\t";
-	try{
-		if(queue.isEmpty()){
-			throw "exception";
-		}
-		else{	
-			cout<<queue.front();
-			cout<<"\t\t";
-			queue.print();					
-			cout<<endl;
-		}
-	}
-	catch(...){
-		cout<<"exception"<<endl;
-	}
+    cout << "front()\t\t\t";
+    try {
+        if (queue.isEmpty()) {
+            throw "exception";
+        } else {
+            cout << queue.front();
+            cout << "\t\t";
+            queue.print();
+            cout << endl;
+        }
+    } catch (...) {
+        cout << "exception" << endl;
+    }
+    
+    return 0;
 }
