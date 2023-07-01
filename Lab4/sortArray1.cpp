d7d41be994977bf3255748dc68d37fb62b28cc37
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 03:51:44 -0700

    Update sortArray1.cpp

--- Lab4/sortArray1.cpp
+++ Lab4/sortArray1.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 
 using namespace std;
+
 int arr_size;
 int order;
 
-void sortArr(int arr[]){
+void sortArr(int arr[])
+{
     int temp = 0;
     int swap_index = 0;
 
-    if(order == 0){ // ascending sort
-        for(int i = 0; i < arr_size; i++){
+    if (order == 0) // ascending sort
+    {
+        for (int i = 0; i < arr_size; i++)
+        {
             swap_index = i;
-            for(int j = i; j < arr_size; j++){
-                if(arr[j] < arr[swap_index]){
+            for (int j = i; j < arr_size; j++)
+            {
+                if (arr[j] < arr[swap_index])
+                {
                     swap_index = j;
                 }
             }
@@ -22,12 +28,15 @@ void sortArr(int arr[]){
         }
         cout << "This is the sorted array in ascending order: ";
     }
-
-    if(order == 1){ // descending sort
-        for(int i = 0; i < arr_size; i++){
+    else if (order == 1) // descending sort
+    {
+        for (int i = 0; i < arr_size; i++)
+        {
             swap_index = i;
-            for(int j = i; j < arr_size; j++){
-                if(arr[swap_index] < arr[j]){
+            for (int j = i; j < arr_size; j++)
+            {
+                if (arr[swap_index] < arr[j])
+                {
                     swap_index = j;
                 }
             }
@@ -37,32 +46,30 @@ void sortArr(int arr[]){
         }
         cout << "This is the sorted array in descending order: ";
     }
+    else
+    {
+        cout << "Invalid sort order. Please enter 0 for ascending or 1 for descending." << endl;
+        return;
+    }
 }
 
-
-int main(){
+int main()
+{
     cout << "Enter the size of the array: ";
     cin >> arr_size;
 
-    if(arr_size < 0){ // error handling
+    if (arr_size < 0) // error handling
+    {
         cout << "ERROR: you entered an incorrect value for the array size!" << endl;
         return 0;
     }
 
     // gets array from user
-    cout << "Enter the numbers in the array, seperated by a space, and press enter: ";
+    cout << "Enter the numbers in the array, separated by a space, and press enter: ";
     int arr[arr_size];
-    for(int i = 0; i < arr_size; i++){
-        scanf("%d", &arr[i]); // accepts integers (+ -) and inputs them into array
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i]; // accepts integers (+ -) and inputs them into array
     }
 
-    cout << "Sort in ascending (0) or descending (1) order? ";
-    cin >> order;
-
-    sortArr(arr);
-    for(int i = 0; i < arr_size; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    return 0;  
-}
+    cout << "Sort in ascending (0) or
