dfbc6fc2585b054f3c308f51f04831b64d89ecab
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 03:50:29 -0700

    Update sortArray1.cpp

--- Lab3/sortArray1.cpp
+++ Lab3/sortArray1.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 
 using namespace std;
+
 int arr_size;
 
-void sort(int arr[]){
+void sort(int arr[])
+{
     cout << "This is the sorted array in descending order: ";
-    for(int i = 0; i < arr_size; i++){
-        int indx = 0;
-        int max = arr[i];
-        for(int j = i; j < arr_size - 1; j++){
-            if(max <= arr[j + 1]){
-                max = arr[j + 1];
-                indx = j + 1;
+    for (int i = 0; i < arr_size; i++)
+    {
+        int max_idx = i;
+        for (int j = i + 1; j < arr_size; j++)
+        {
+            if (arr[j] > arr[max_idx])
+            {
+                max_idx = j;
             }
         }
-        int temp = arr[i];
-        arr[i] = max;
-        arr[indx] = temp;
+        swap(arr[i], arr[max_idx]);
         cout << arr[i] << " ";
     }
     cout << endl;
     cout << "The algorithm selected the maximum for the traverse of the array." << endl;
 }
 
-
-int main(){
+int main()
+{
     cout << "Enter the size of the array: ";
     cin >> arr_size;
 
-    if(arr_size < 0){ // error handling
+    if (arr_size < 0)
+    {
         cout << "ERROR: you entered an incorrect value for the array size!" << endl;
         return 0;
     }
 
-    // gets array from user
-    cout << "Enter the numbers in the array, seperated by a space, and press enter: ";
+    cout << "Enter the numbers in the array, separated by a space, and press enter: ";
     int arr[arr_size];
-    for(int i = 0; i < arr_size; i++){
-        scanf("%d", &arr[i]); // accepts integers (+ -) and inputs them into array
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i];
     }
     sort(arr);
-    return 0;  
+    return 0;
 }
