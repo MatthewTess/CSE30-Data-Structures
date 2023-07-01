8e8ec2bac4b00e6b758ecad9cc202d2adbd41d6e
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 04:00:00 -0700

    Update Exercise2.cpp

--- Lab8/Exercise2.cpp
+++ Lab8/Exercise2.cpp
@@ -3,25 +3,23 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     LinkedList firstList;
     LinkedList secondList;
 
     // Check if the lists are empty
-    if(firstList.isEmpty())
+    if (firstList.isEmpty())
         cout << "The first list is empty!" << endl;
     else
-        cout << "The first list is NOT empty..." << endl;    
+        cout << "The first list is NOT empty..." << endl;
 
-    if(secondList.isEmpty())
+    if (secondList.isEmpty())
         cout << "The second list is empty!" << endl;
     else
         cout << "The second list is NOT empty..." << endl;
 
-
     // Print the size of the lists
-    cout << "The size of the first list is: " << firstList.size() << endl; 
+    cout << "The size of the first list is: " << firstList.size() << endl;
     cout << "The size of the second list is: " << secondList.size() << endl;
 
     // Insert some values into the first list
@@ -31,24 +29,36 @@ int main()
     firstList.insertAtFront(4);
     firstList.insertAtFront(5);
 
-    // Print the lists    
-    cout << "Here is the first list: ["; firstList.print(); cout << "]" << endl;
-    cout << "Here is the second list: ["; secondList.print(); cout << "]" << endl;
+    // Print the lists
+    cout << "Here is the first list: [";
+    firstList.print();
+    cout << "]" << endl;
+    cout << "Here is the second list: [";
+    secondList.print();
+    cout << "]" << endl;
 
     // Insert a value into the second list
     secondList.insertAtFront(25);
 
     // Print the lists
-    cout << "Here is the first list: ["; firstList.print(); cout << "]" << endl;
-    cout << "Here is the second list: ["; secondList.print(); cout << "]" << endl;
+    cout << "Here is the first list: [";
+    firstList.print();
+    cout << "]" << endl;
+    cout << "Here is the second list: [";
+    secondList.print();
+    cout << "]" << endl;
 
     // Clear the second list and remove an item from the first list
     secondList.clear();
     firstList.removeFromFront();
 
     // Print the lists
-    cout << "Here is the first list: ["; firstList.print(); cout << "]" << endl;
-    cout << "Here is the second list: ["; secondList.print(); cout << "]" << endl;
+    cout << "Here is the first list: [";
+    firstList.print();
+    cout << "]" << endl;
+    cout << "Here is the second list: [";
+    secondList.print();
+    cout << "]" << endl;
 
     // Clear the first list and add five items into the second list
     firstList.clear();
@@ -58,52 +68,12 @@ int main()
     secondList.insertAtFront(10);
 
     // Print the lists
-    cout << "Here is the first list: ["; firstList.print(); cout << "]" << endl;
-    cout << "Here is the second list: ["; secondList.print(); cout << "]" << endl;
+    cout << "Here is the first list: [";
+    firstList.print();
+    cout << "]" << endl;
+    cout << "Here is the second list: [";
+    secondList.print();
+    cout << "]" << endl;
 
     // Print the size of the lists
-    cout << "The size of the first list is: " << firstList.size() << endl;
-    cout << "The size of the second list is: " << secondList.size() << endl;
-
-    // Check if the lists are empty
-    if(firstList.isEmpty())
-        cout << "The first list is empty!" << endl;
-    else
-        cout << "The first list is NOT empty..." << endl;
-
-    if(secondList.isEmpty())
-        cout << "The second list is empty!" << endl;
-    else
-        cout << "The second list is NOT empty..." << endl;
-
-    // Remove all the elements of the second list using a for loop
-    for(int i=0; i<6; i++)
-    {
-        cout << "Here is the second list: ["; secondList.print(); cout << "]" << endl;
-
-        if(secondList.removeFromFront())        
-            cout << "Successfully removed an item from the list..." << endl;
-        else
-            cout << "COULD NOT remove an item from the list!" << endl;
-    }
-
-    // Print the size of the lists
-    cout << "The size of the first list is: " << firstList.size() << endl;
-    cout << "The size of the second list is: " << secondList.size() << endl;
-
-    // Check if the lists are empty
-    if(firstList.isEmpty())
-        cout << "The first list is empty!" << endl;
-    else
-        cout << "The first list is NOT empty..." << endl;
-
-    if(secondList.isEmpty())
-        cout << "The second list is empty!" << endl;
-    else
-        cout << "The second list is NOT empty..." << endl;
-
-
-    return 1;
-}
-
-
+    cout << "The size of the first list is: " << firstList.size() <<
